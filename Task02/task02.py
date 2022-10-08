ea81e634e4eea9db3c4d779665ba1cac87dd293b
Vera Krasulina <kshisinka@hotmail.com>
2022-10-08 17:44:47 +0300

    Task03

--- Task02/task02.py
+++ Task02/task02.py
@@ -4,7 +4,7 @@
 n = 0
 while(n <=7):
     comb = [int(i) for i in str(f'{n:03b}')]
-    print('X = ', comb[0], 'Y = ', comb[1], 'Z =', comb[2])
+    print('X =', comb[0], 'Y =', comb[1], 'Z =', comb[2])
     if (not(comb[0] or comb[1] or comb[2]) == (not(comb[0]) and not (comb[1]) and not(comb[2]))):
        print(True)
     else: print (False)
